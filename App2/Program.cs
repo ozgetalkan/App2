34d89b159773bb9e2f6e0c178d326d372b38fe50
özge alkan <ozgetanulkualkan@gmail.com>
2023-01-10 15:49:42 +0300

    Add project files.

---
+++ App2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace App2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public class Yazici
+    {
+        public Yazici(string name)
+        {
+            Name = name;
+            OlusturmaTarihi = System.DateTime.Now;
+            UId = System.Guid.NewGuid().ToString();
+        }
+        public int Id { get; set; }
+        public string UId { get; set; }
+        public DateTime OlusturmaTarihi { get; set; }
+        public string Name { get; set; }
+    }
+}
